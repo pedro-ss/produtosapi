9448ab443946def6f6a844475f682a80edad62e0
pedro-ss <pedross.tool@gmail.com>
2019-07-14 21:14:28 -0300

    Simple api

---
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -0,0 +1,62 @@
+package com.produtos.apirest.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.produtos.apirest.models.Produto;
+import com.produtos.apirest.repository.ProdutoRepository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@RestController
+@RequestMapping(value="/api")
+@Api("API REST Produtos")
+@CrossOrigin(origins="*") //API pública
+public class ProdutoResource {
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
+	@GetMapping("/produtos")
+	@ApiOperation(value="Retorna uma lista de produtos")
+	public List<Produto> listaProduto(){
+		
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping("/produto/{id}")
+	@ApiOperation(value="Retorna um produto único")
+	public Produto listaProdutoUnico(@PathVariable(value="id") long id){
+		
+		return produtoRepository.findById(id);
+	}
+	
+	@PostMapping("/produto")
+	@ApiOperation(value="Salva um produto")
+	public Produto salvaProduto(@RequestBody Produto produto){
+		return produtoRepository.save(produto);
+	}
+	
+	@DeleteMapping("/produto")
+	@ApiOperation(value="Deleta um produto")
+	public Produto deletaProduto(@PathVariable(value="id") long id){
+		return produtoRepository.deleteById(id);
+	}
+	
+	@PutMapping("/produto")
+	@ApiOperation(value="Atualiza um produto")
+	public Produto atualizaProduto(@RequestBody Produto produto){
+		return produtoRepository.save(produto);
+	}
+}
